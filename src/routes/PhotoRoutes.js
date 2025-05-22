6512f685cb173ac7d01d165b533b6b39182c2f4e
Marcos Schlick <marcosschlick@gmail.com>
2025-05-22 17:07:29 -0300

    feat: add PhotoRoutes

---
+++ src/routes/PhotoRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { CompetitionPhotoController } from "../app/controllers/CompetitionPhotoController.js";
+
+const photoController = new CompetitionPhotoController();
+const router = express.Router();
+
+// photos routes
+router.get("", (req, res) => photoController.findAll(req, res));
+router.get("/:photoId", (req, res) => photoController.findById(req, res));
+
+export { router as photoRoutes };
