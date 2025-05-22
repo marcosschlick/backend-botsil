6107d64f375d15d24d087e2769a3db2e6c9e9b08
Marcos Schlick <marcosschlick@gmail.com>
2025-05-22 17:07:41 -0300

    chore: register article, manual and photo routes in app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import express from "express";
+import { articleRoutes } from "./routes/ArticleRoutes.js";
+import { manualRoutes } from "./routes/ManualRoutes.js";
 import { personRoutes } from "./routes/PersonRoutes.js";
+import { photoRoutes } from "./routes/PhotoRoutes.js";
 
 const app = express();
 app.use(express.json());
 
+app.use("/articles", articleRoutes);
+app.use("/manuals", manualRoutes);
 app.use("/members", personRoutes);
+app.use("/photos", photoRoutes);
 
 export default app;
