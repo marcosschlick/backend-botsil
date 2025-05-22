c6c01c5d3ddf4aba416e6f3c87d242a2fe9b98e7
Marcos Schlick <marcosschlick@gmail.com>
2025-05-22 16:09:21 -0300

    feat: add Person entity class

---
+++ src/app/entities/Person.js
@@ -0,0 +1,10 @@
+export class Person {
+  constructor({ id, full_name, email, phone, birth_date, profile_image_url }) {
+    this.id = id;
+    this.full_name = full_name;
+    this.email = email;
+    this.phone = phone;
+    this.birth_date = birth_date;
+    this.profile_image_url = profile_image_url;
+  }
+}
