f5b737cb24a5d8f64c638a1f3e92ef5df260d47e
Marcos Schlick <marcosschlick@gmail.com>
2025-05-22 17:04:02 -0300

    feat: add Manual entity

---
+++ src/app/entities/Manual.js
@@ -0,0 +1,8 @@
+export class Manual {
+  constructor({ id, modality_id, post_date, document_url }) {
+    this.id = id;
+    this.modality_id = modality_id;
+    this.post_date = post_date;
+    this.document_url = document_url;
+  }
+}
