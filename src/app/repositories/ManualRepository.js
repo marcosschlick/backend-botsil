71e5a89f8042d3ce334a044f173a7a32b25a63ab
Marcos Schlick <marcosschlick@gmail.com>
2025-05-22 17:04:35 -0300

    feat: add ManualRepository

---
+++ src/app/repositories/ManualRepository.js
@@ -0,0 +1,16 @@
+import pool from "../../database/database.js";
+import { Manual } from "../entities/Manual.js";
+
+export class ManualRepository {
+  async findAll() {
+    const query = "SELECT * FROM manual";
+    const { rows } = await pool.query(query);
+    return rows.map((row) => new Manual(row));
+  }
+
+  async findById(id) {
+    const query = "SELECT * FROM manual WHERE id = $1";
+    const { rows } = await pool.query(query, [id]);
+    return rows[0] ? new Manual(rows[0]) : null;
+  }
+}
