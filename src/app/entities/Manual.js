cf7d6e2e4853795e086c021694905ef376696d76
Marcos Schlick <marcosschlick@gmail.com>
2025-05-27 17:26:04 -0300

    refactor: simplify core entity classes for MVP

--- src/app/entities/Manual.js
+++ src/app/entities/Manual.js
@@ -1,8 +1,6 @@
 export class Manual {
-  constructor({ id, modality_id, post_date, document_url }) {
+  constructor({ id, manual_url }) {
     this.id = id;
-    this.modality_id = modality_id;
-    this.post_date = post_date;
-    this.document_url = document_url;
+    this.document_url = manual_url;
   }
 }
--- src/app/entities/Person.js
+++ src/app/entities/Person.js
@@ -1,10 +1,7 @@
 export class Person {
-  constructor({ id, full_name, email, phone, birth_date, profile_image_url }) {
+  constructor({ id, name, profile_photo_url }) {
     this.id = id;
-    this.full_name = full_name;
-    this.email = email;
-    this.phone = phone;
-    this.birth_date = birth_date;
-    this.profile_image_url = profile_image_url;
+    this.name = name;
+    this.photo_url = profile_photo_url;
   }
 }
