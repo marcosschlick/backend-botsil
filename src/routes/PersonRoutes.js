76713805ad8061cdd7e7fc19845b20fbd31c41f7
Marcos Schlick <marcosschlick@gmail.com>
2025-05-27 17:26:47 -0300

    refactor: reorganize routes for essential endpoints

--- src/routes/PersonRoutes.js
+++ src/routes/PersonRoutes.js
@@ -5,8 +5,6 @@ const personController = new PersonController();
 const router = express.Router();
 
 // members routes
-router.post("", (req, res) => personController.create(req, res));
 router.get("", (req, res) => personController.findAll(req, res));
-router.get("/:personId", (req, res) => personController.findById(req, res));
 
 export { router as personRoutes };
