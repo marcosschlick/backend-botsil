88b9cd67ebf7fc9e92fb4238c4a04b8f4edbf31e
Marcos Schlick <marcosschlick@gmail.com>
2025-05-27 17:26:14 -0300

    refactor: streamline repositories for essential functionality

--- src/app/repositories/CompetitionPhotoRepository.js
+++ src/app/repositories/CompetitionPhotoRepository.js
@@ -2,31 +2,9 @@ import pool from "../../database/database.js";
 import { CompetitionPhoto } from "../entities/CompetitionPhoto.js";
 
 export class CompetitionPhotoRepository {
-  async create(photoData) {
-    const query = `
-    INSERT INTO competition_photo (id, competition_id, photo_url, description)
-    VALUES ($1, $2, $3, $4)
-    RETURNING *
-  `;
-    const values = [
-      photoData.id,
-      photoData.competition_id,
-      photoData.photo_url,
-      photoData.description,
-    ];
-    const { rows } = await pool.query(query, values);
-    return rows[0] ? new CompetitionPhoto(rows[0]) : null;
-  }
-
   async findAll() {
-    const query = "SELECT * FROM competition_photo";
+    const query = "SELECT * FROM competition_photos";
     const { rows } = await pool.query(query);
     return rows.map((row) => new CompetitionPhoto(row));
-  }
-
-  async findById(id) {
-    const query = "SELECT * FROM competition_photo WHERE id = $1";
-    const { rows } = await pool.query(query, [id]);
-    return rows[0] ? new CompetitionPhoto(rows[0]) : null;
   }
 }
