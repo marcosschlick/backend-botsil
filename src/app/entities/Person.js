0642c73cb3e16fdbb9b1ffe09603ea35ce641437
Marcos Schlick <marcosschlick@gmail.com>
2025-05-29 18:18:10 -0300

    feat: add role property to Person entity

--- src/app/entities/Person.js
+++ src/app/entities/Person.js
@@ -1,7 +1,8 @@
 export class Person {
-  constructor({ id, name, profile_photo_url }) {
+  constructor({ id, name, role, profile_photo_url }) {
     this.id = id;
     this.name = name;
+    this.role = role;
     this.photo_url = profile_photo_url;
   }
 }
