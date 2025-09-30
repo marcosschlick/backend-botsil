cf15cf00da464fc43d79a00de344b3de4b0b220d
Marcos Schlick <marcosschlick@gmail.com>
2025-09-30 12:21:36 -0300

    chore(cleanup): remove obsolete modules and files

--- src/app/entities/Article.js
+++
@@ -1,7 +0,0 @@
-export class Article {
-  constructor({ id, title, article_url }) {
-    this.id = id;
-    this.title = title;
-    this.article_url = article_url;
-  }
-}
--- src/app/entities/Person.js
+++
@@ -1,7 +0,0 @@
-export class Person {
-  constructor({ id, name, profile_photo_url }) {
-    this.id = id;
-    this.name = name;
-    this.photo_url = profile_photo_url;
-  }
-}
